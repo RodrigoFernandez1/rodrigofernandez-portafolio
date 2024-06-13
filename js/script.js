02bb0a9229b41e40da96b0af955fd52709e2f5e9
rodrigoo.fernandezz <rodrigoo1.fernandezz@gmail.com>
2024-06-13 16:07:58 -0300

    meojoras, añadi la parte de proyectos

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gallery = document.querySelector(".galeri-protec");
+  const images = gallery.querySelectorAll(".galeri");
+  let index = 0;
+
+  function showImage(indexToShow) {
+    // Ocultar todas las imágenes
+    images.forEach((image) => {
+      image.style.display = "none";
+    });
+
+    // Mostrar la imagen deseada
+    images[indexToShow].style.display = "block";
+  }
+
+  function nextImage() {
+    index++;
+    if (index >= images.length) {
+      index = 0;
+    }
+    showImage(index);
+  }
+
+  // Mostrar la primera imagen al cargar la página
+  showImage(index);
+
+  // Cambiar de imagen cada 5 segundos
+  setInterval(nextImage, 5000);
+});
